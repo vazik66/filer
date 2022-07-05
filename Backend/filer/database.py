3966c098f13a09dfa4435577b59b118f0f7b2b3e
vazik66 <vasilkov.dev@gmail.com>
2022-07-05 22:33:58 +0500

    Changed abs db path to rel

--- Backend/filer/database.py
+++ Backend/filer/database.py
@@ -1,24 +1,3 @@
-# from file import File
-#
-# db = {}
-#
-#
-# def get_by_url_id(id: str) -> File | None:
-#     row = db.get(id)
-#     return row
-#
-#
-# def create(file: File) -> File:
-#     row = db.get(file.uid)
-#     if row:
-#         raise BaseException("File with such id already exists")
-#
-#     db[file.uid] = file
-#     return file
-#
-#
-# def get_all() -> list[File]:
-#     return [v.json() for _, v in db.items()]
 import sqlite3
 
 import aiosqlite
@@ -26,7 +5,7 @@ import os
 from logger import get_logger
 
 
-db_path = r'C:\Users\andrew\Desktop\Code\python\filer\backend\database.db'
+db_path = 'database.db'
 logger = get_logger('database')
 
 
