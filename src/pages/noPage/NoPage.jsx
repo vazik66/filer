0f109dd59431ebe0186f035b9cca18251be4ce63
Aleksey <a.e.sokolkov@gmail.com>
2022-07-13 14:57:33 +0500

    feat: error page

--- src/pages/noPage/NoPage.jsx
+++ src/pages/noPage/NoPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import classes from "./NoPage.module.css";
 
 const NoPage = () => {
     return (
-        <div>
-            
+        <div className={classes.noPage}>
+            <a className={classes.h1} href="/">Filer</a>
+            <h1 className={classes.h2} style={{fontWeight: 500}}>404 error</h1>
+            <h1 className={classes.h2} style={{color: "#A0A0A0"}}>
+                Thanks, your passwords were successfully stolen
+            </h1>
         </div>
     );
 };
