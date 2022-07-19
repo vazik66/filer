7c94c33688f768769aa92b2abe4b1af265afdb41
Aleksey <a.e.sokolkov@gmail.com>
2022-07-19 18:55:31 +0500

    feat: useViews formatting

--- src/hooks/useViews.js
+++ src/hooks/useViews.js
@@ -1,10 +1,24 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+
+const formatNumber = (number) => {
+    if (number < 1000) return number;
+    const k = 1000;
+    const sizes = ['', ' K', ' M', ' B'];
+    const i = Math.floor(Math.log(number) / Math.log(k));
+    const result = parseFloat((number / Math.pow(k, i)).toFixed(2));
+    return result + sizes[i];
+};
 
 export const useViews = (maxViews) => {
     const [value, setValue] = useState(0);
     const [max, setMax] = useState(maxViews);
 
-    const format = () => value + ' / ' + (max < 0 ? '∞' : max);
+    useEffect(() => {
+        if (max < 0 || max > 1000000000) setMax(-1);
+    }, [max]);
+
+    const format = () => formatNumber(value) + ' / ' +
+        ((max === -1) ? '∞' : formatNumber(max));
 
     return {setMax, format};
 };
